0e37c6ea1a35b6e19e3b171f32f61f8d8e1cd5de
Selvamariyappan S <selvamariyappan2@gmail.com>
2025-08-01 20:08:16 +0530

    After Modify

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function showResult() {
-    const input1 = document.getElementById('input1').value;
-    const input2 = document.getElementById('input2').value;
+    const input1 = parseFloat(document.getElementById('input1').value);
+    const input2 = parseFloat(document.getElementById('input2').value);
     const resultDiv = document.getElementById('result');
-    resultDiv.textContent = `Result: ${input1} ${input2}`;
+    if (!isNaN(input1) && input1 !== 0 && !isNaN(input2)) {
+        const result = (input2 / input1) * 100;
+        const roundedResult = (result % 1 >= 0.5) ? Math.ceil(result) : Math.floor(result);
+        resultDiv.textContent = `Overall percentage : ${roundedResult}%`;
+        resultDiv.style.display = 'inline-block';
+    } else {
+        resultDiv.textContent = 'Please enter valid numbers (input 1 must not be zero).';
+        resultDiv.style.display = 'inline-block';
+    }
 }
